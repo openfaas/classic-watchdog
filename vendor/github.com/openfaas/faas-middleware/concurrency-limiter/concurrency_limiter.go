b20362a05bde813764e0b20d27de58df2159bc0a
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2023-11-10 10:38:05 +0000

    Update faas-middleware and Go version to 1.21
    The new version of faas-middleware adds an X-OpenFaaS-Internal
    header when maximum concurrency is met due to max_inflight.
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- vendor/github.com/openfaas/faas-middleware/concurrency-limiter/concurrency_limiter.go
+++ vendor/github.com/openfaas/faas-middleware/concurrency-limiter/concurrency_limiter.go
@@ -6,6 +6,11 @@ import (
 	"sync/atomic"
 )
 
+// Limiter is an interface that can be used to check if a limit has been met.
+type Limiter interface {
+	Met() bool
+}
+
 type ConcurrencyLimiter struct {
 	backendHTTPHandler http.Handler
 	/*
@@ -38,30 +43,62 @@ type ConcurrencyLimiter struct {
 	maxInflightRequests uint64
 }
 
+func (cl *ConcurrencyLimiter) Met() bool {
+	// We should not have any ConcurrencyLimiter created with a limit of 0
+	// but return early if that's the case.
+	if cl.maxInflightRequests == 0 {
+		return false
+	}
+
+	requestsStarted := atomic.LoadUint64(&cl.requestsStarted)
+	completedRequested := atomic.LoadUint64(&cl.requestsCompleted)
+	return requestsStarted-completedRequested >= cl.maxInflightRequests
+}
+
 func (cl *ConcurrencyLimiter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// We should not have any ConcurrencyLimiter created with a limit of 0
+	// but we'll check anyway and return early.
+	if cl.maxInflightRequests == 0 {
+		cl.backendHTTPHandler.ServeHTTP(w, r)
+		return
+	}
+
 	requestsStarted := atomic.AddUint64(&cl.requestsStarted, 1)
 	completedRequested := atomic.LoadUint64(&cl.requestsCompleted)
 	if requestsStarted-completedRequested > cl.maxInflightRequests {
 		// This is a failure pathway, and we do not want to block on the write to finish
 		atomic.AddUint64(&cl.requestsCompleted, 1)
+
+		// Some APIs only return JSON, since we can interfere here and send a plain/text
+		// message, let's do the right thing so that downstream users can consume it.
+		w.Header().Add("Content-Type", "text/plain")
+		w.Header().Add("X-OpenFaaS-Internal", "faas-middleware")
+
 		w.WriteHeader(http.StatusTooManyRequests)
+
 		fmt.Fprintf(w, "Concurrent request limit exceeded. Max concurrent requests: %d\n", cl.maxInflightRequests)
 		return
 	}
+
 	cl.backendHTTPHandler.ServeHTTP(w, r)
 	atomic.AddUint64(&cl.requestsCompleted, 1)
 }
 
-// NewConcurrencyLimiter creates a handler which limits the active number of active, concurrent
-// requests. If the concurrency limit is less than, or equal to 0, then it will just return the handler
+// NewConcurrencyLimiter creates NewConcurrencyLimiter with a Handler() function that returns a
+// handler which limits the active number of active, concurrent requests.
+//
+// If the concurrency limit is less than, or equal to 0, then it will just return the handler
 // passed to it.
-func NewConcurrencyLimiter(handler http.Handler, concurrencyLimit int) http.Handler {
-	if concurrencyLimit <= 0 {
-		return handler
-	}
-
+//
+// The Met() function will return true if the concurrency limit is exceeded within the handler
+// at the time of the call.
+func NewConcurrencyLimiter(handler http.Handler, concurrencyLimit int) *ConcurrencyLimiter {
 	return &ConcurrencyLimiter{
 		backendHTTPHandler:  handler,
 		maxInflightRequests: uint64(concurrencyLimit),
 	}
+}
+
+func (cl *ConcurrencyLimiter) Handler() http.Handler {
+	return cl
 }
